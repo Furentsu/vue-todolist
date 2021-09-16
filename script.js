0e1433dfcee3f6837e062028cf35fc9b31516b4c
Furentsu <eugeniofranzoso@gmail.com>
2021-09-16 15:03:59 +0200

    Initialization

---
+++ script.js
@@ -0,0 +1,17 @@
+// Stampare in pagina un item per ogni elemento contenuto in un array.
+
+// Ogni item ha una icona "x" associata: cliccando su di essa, l'item viene rimosso dalla lista.
+
+// Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure 
+// ciccando su un pulsante, il testo digitato sarà aggiunto alla lista.
+
+const root = new Vue({
+    el: 'root',
+    data: {
+        
+    },
+    methods: {
+
+    },
+    // Hooks
+})
